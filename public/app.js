14483525bbc1d28846e8227dedbd83ff0da5afd5
leedd87 <leedav87@gmail.com>
2023-08-17 15:03:36 -0300

    interface and functions to tweet project

--- public/app.js
+++ public/app.js
@@ -1,2 +1,18 @@
 "use strict";
 //Crear unos tipos de datos y interfaces
+function createMainTweet() {
+    const id = crypto.randomUUID();
+    const tweet = createTweet();
+    return {
+        id,
+        tweets: [tweet]
+    };
+}
+function createTweet() {
+    const id = crypto.randomUUID();
+    const message = '';
+    return {
+        id,
+        message
+    };
+}
--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,28 @@ interface Tweet {
     id: Id,
     message: Message
 }
+
+interface TweetView {
+    id: Id,
+    tweets: Tweet[];
+}
+
+function createMainTweet(): TweetView {
+    const id = crypto.randomUUID();
+    const tweet = createTweet()
+
+    return {
+        id,
+        tweets: [tweet]
+    }
+}
+
+function createTweet(): Tweet {
+    const id = crypto.randomUUID();
+    const message = '';
+
+    return {
+        id,
+        message
+    }
+}
