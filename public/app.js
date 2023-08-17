b99db491c770b1e5cc9392627093c46b83ee0b69
leedd87 <leedav87@gmail.com>
2023-08-17 15:13:50 -0300

    add renderTweet

--- public/app.js
+++ public/app.js
@@ -17,6 +17,7 @@ function createTweet() {
     };
 }
 function renderView(tweetView) {
+    var _a;
     let view = document.querySelector('#container-' + tweetView.id);
     if (view) {
         view.innerHTML = '';
@@ -24,5 +25,26 @@ function renderView(tweetView) {
     else {
         view = document.createElement('div');
         view.id = 'container' + tweetView.id;
+        view.classList.add('mainContainer');
+        (_a = document.querySelector('#tweets')) === null || _a === void 0 ? void 0 : _a.append(view);
     }
+    for (const tweet of tweetView.tweets) {
+        //renderTweet
+    }
+}
+function renderTweet(tweetView, view, tweet) {
+    const tweetContainer = document.createElement('div');
+    tweetContainer.id = 'container-' + tweet.id;
+    tweetContainer.classList.add('tweetContainer');
+    const form = document.createElement('form');
+    form.id = 'form-' + tweet.id;
+    tweetContainer.appendChild(form);
+    const textarea = document.createElement('textarea');
+    textarea.id = 'textarea' + tweet.id;
+    textarea.value = tweet.message;
+    textarea.maxLength = 250;
+    const buttonAddMore = document.createElement('button');
+    buttonAddMore.classList.add('button', 'buttonNew');
+    buttonAddMore.value = 'Add another tweets';
+    buttonAddMore.append(document.createTextNode('Add another tweet'));
 }
--- src/app.ts
+++ src/app.ts
@@ -43,6 +43,34 @@ function renderView(tweetView: TweetView) {
     } else {
         view = document.createElement('div');
         view.id = 'container' + tweetView.id;
+        view.classList.add('mainContainer');
+        document.querySelector('#tweets')?.append(view);
     }
 
+    for (const tweet of tweetView.tweets) {
+        //renderTweet
+    }
+
+
+
+}
+
+function renderTweet(tweetView: TweetView, view: HTMLDivElement, tweet: Tweet) {
+    const tweetContainer = document.createElement('div');
+    tweetContainer.id = 'container-' + tweet.id;
+    tweetContainer.classList.add('tweetContainer');
+
+    const form = document.createElement('form');
+    form.id = 'form-' + tweet.id;
+    tweetContainer.appendChild(form)
+
+    const textarea = document.createElement('textarea');
+    textarea.id = 'textarea' + tweet.id;
+    textarea.value = tweet.message;
+    textarea.maxLength = 250;
+
+    const buttonAddMore = document.createElement('button');
+    buttonAddMore.classList.add('button', 'buttonNew');
+    buttonAddMore.value = 'Add another tweets';
+    buttonAddMore.append(document.createTextNode('Add another tweet'))
 }
